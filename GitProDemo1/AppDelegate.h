fa70ff0c4465327fbc02cdb15e1a20751c4f81f6
dqw <dqwdlut@qq.com>
2015-12-27 11:32:43 +0800

    Initial Commit

---
+++ GitProDemo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitProDemo1
+//
+//  Created by dqw on 15/12/27.
+//  Copyright © 2015年 zmjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitProDemo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitProDemo1
+//
+//  Created by dqw on 15/12/27.
+//  Copyright © 2015年 zmjr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
